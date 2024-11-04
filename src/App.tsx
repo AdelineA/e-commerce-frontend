15872471386ec2112f836aa99a085a20e9982997
AdelineA <adelinemug6@gmail.com>
2024-11-04 16:21:41 +0200

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Components/Routes/Routes";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
