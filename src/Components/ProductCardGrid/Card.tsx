20f92c93ba12c5e98ab91b56c453cc47bb28fab5
AdelineA <adelinemug6@gmail.com>
2024-11-12 10:06:37 +0200

    feat: add wishlist and view svgs

--- src/Components/ProductCardGrid/Card.tsx
+++ src/Components/ProductCardGrid/Card.tsx
@@ -1,3 +1,5 @@
+import View from "../../assets/Icons/View";
+import WishList from "../../assets/Icons/Wishlist";
 import type { ProductCard } from "../../types/Product";
 
 const ProductCard = ({
@@ -10,7 +12,15 @@ const ProductCard = ({
   return (
     <div className="rounded-md shadow-md">
       <div className="bg-gray-100 flex justify-center h-2/3 py-5">
-        <img src={image} alt="product" width={240} />
+        <img src={image} alt="product" width={200} />
+        <div className="flex flex-col gap-4 relative top-0 -right-10 items-center">
+          <button className="bg-white p-2 rounded-full w-10 h-10">
+            <WishList />
+          </button>
+          <button className="bg-white p-2 rounded-full h-10 w-10">
+            <View />
+          </button>
+        </div>
       </div>
       <div className="p-2">
         <h3>{title}</h3>
