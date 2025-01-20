1cdccbcc38ec08b8df558586e9197fcd2d212be4
AdelineA <adelinemug6@gmail.com>
2025-01-20 13:08:31 +0200

    feat: add products to cart

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -7,16 +7,20 @@ const Shop = () => {
 
   return (
     <div>
-      <h1>Cart</h1>
+      <h1 className="py-10">Cart</h1>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <div className="flex gap-10">
+        <div className="grid xl:grid-cols-3 mg:grid-col-2 grid-cols-1 gap-4">
           {cart.map((product: ProductCard) => (
-            <div key={product.id} className="bg-gray-100 p-4 rounded-md shadow">
+            <div key={product.id} className="p-4 rounded-md shadow-md h-2/3">
               <h2>{product.quantity}</h2>
-              <img src={product.imageUrl} alt={product.title} />
-              <h3>{product.title}</h3>
+              <div className="flex justify-center h-1/2">
+                <img src={product.imageUrl} alt={product.title} />
+              </div>
+              <h3 className="pt-4 text-base font-semibold text-center">
+                {product.title}
+              </h3>
               <p>${product.price * (product.quantity ?? 1)}</p>
             </div>
           ))}
